debc90241284b02028d0368d24a51e6ec33efe50
vinicius <niviciusdev@gmail.com>
2025-06-16 00:22:14 -0300

    Add initial UserRepository stub and remove .gitkeep files

---
+++ src/repositories/user.repository.ts
@@ -0,0 +1,8 @@
+// TODO: implement user repository
+import { PrismaClient } from '@prisma/client';
+import { CreateUserData, UpdateUserData } from '../types/user.types';
+import { prisma } from '../utils/prisma';
+
+export class UserRepository {
+  // Implement
+}

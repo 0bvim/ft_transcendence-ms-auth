65b5b6cdd9f1e99de888995be8795494a2a10ed5
vinicius <niviciusdev@gmail.com>
2025-06-26 23:43:44 -0300

    Prevent duplicate email and username on registration

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,6 +1,7 @@
 import { hash } from "bcryptjs";
 import { User } from "@prisma/client";
 import { UsersRepository } from "../repositories/users-repository";
+import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
 
 interface RegisterUseCaseRequest {
   username: string;
@@ -21,6 +22,20 @@ export class RegisterUseCase {
     email,
     password,
   }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
+    // check if email already exists
+    const userWithSameEmail = await this.usersRepository.findByEmail(email);
+
+    if (userWithSameEmail) {
+      throw new UserAlreadyExistsError();
+    }
+
+    // check if username already exists
+    const userWithSameUsername =
+      await this.usersRepository.findByUsername(username);
+
+    if (userWithSameUsername) {
+      throw new UserAlreadyExistsError();
+    }
     // Hash the password. 6 is the salt rounds
     // TODO: store the salt rounds in an environment variable
     const password_hash = await hash(password, 6);
